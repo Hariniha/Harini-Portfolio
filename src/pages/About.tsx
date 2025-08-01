2f20645660878df516343ba00ea6f7e26de0d8a5
Hariniha <harinikannan302004@gmail.com>
2025-08-01 09:39:30 +0530

    commit

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -109,10 +109,10 @@ const About = () => {
                     </div>
                 </div>
 
-                <ScrollVelocity
-                    texts={['HTML5', 'CSS3', 'JavaScript', 'TypeScript', 'TailwindCSS', 'NextJS', 'ReactJS', 'ViteJS', 'AnimeJS', 'Framer-Motion', 'Canva', 'Figma', 'Adobe Xd', 'Solidity', 'Web3JS', 'Vercel', 'Digital Ocean', 'Shadcn', 'GSAP', 'NodeJS', 'ExpressJS', 'MongoDB', 'React Router', 'Redux']}
-                    velocity={10}
-                    className="custom-scroll-text wendy-font text-base md:text-2xl bg-[#121212] w-fit rounded-3xl p-3 md:p-6 border border-white/[0.15] shadow-inner hover:shadow-amber-500/50 transition-all duration-300 m-4"
+                 <ScrollVelocity
+                    texts={['HTML5', 'CSS3', 'JavaScript', 'TypeScript', 'TailwindCSS', 'NextJS', 'ReactJS', 'ViteJS',  'Canva', 'Vercel', 'Shadcn',  'NodeJS', 'ExpressJS', 'MongoDB', 'React Router']}
+                    velocity={-10}
+                    className="custom-scroll-text text-base md:text-2xl wendy-font bg-[#121212] w-fit rounded-3xl p-3 md:p-6 border border-white/[0.15] shadow-inner hover:shadow-amber-500/50 transition-all duration-300 m-4"
                 />
                 <div className="my-10 max-w-[800px] mx-auto">
                     <ScrollReveal
